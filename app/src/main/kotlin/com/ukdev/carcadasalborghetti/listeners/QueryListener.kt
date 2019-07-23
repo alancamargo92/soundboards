6ce090126a36a7378be22cc88524b3e99edeaa2c
Alan Camargo <alan.uk.br@gmail.com>
2019-07-23 20:26:08 +0100

    Generify QueryListener

--- app/src/main/kotlin/com/ukdev/carcadasalborghetti/listeners/QueryListener.kt
+++ app/src/main/kotlin/com/ukdev/carcadasalborghetti/listeners/QueryListener.kt
@@ -1,15 +1,15 @@
 package com.ukdev.carcadasalborghetti.listeners
 
 import androidx.appcompat.widget.SearchView
-import com.ukdev.carcadasalborghetti.adapter.AudioAdapter
-import com.ukdev.carcadasalborghetti.model.Audio
+import com.ukdev.carcadasalborghetti.adapter.MediaAdapter
+import com.ukdev.carcadasalborghetti.model.Media
 
-class QueryListener(private val adapter: AudioAdapter,
-                    private val audio: List<Audio>) : SearchView.OnQueryTextListener {
+class QueryListener<T: Media>(private val adapter: MediaAdapter<T>,
+                              private val medias: List<T>) : SearchView.OnQueryTextListener {
     override fun onQueryTextSubmit(query: String?) = false
 
     override fun onQueryTextChange(newText: String?): Boolean {
-        adapter.filter(audio, newText)
+        adapter.filter(medias, newText)
         return false
     }
 }
