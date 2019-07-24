a8b5a60acb306ef335b910b900de717d1fb4cbc3
Alan Camargo <alan.uk.br@gmail.com>
2019-07-24 19:52:50 +0100

    Implementing free version video fragment

--- app/src/free/kotlin/com/ukdev/carcadasalborghetti/fragments/VideoFragment.kt
+++ app/src/free/kotlin/com/ukdev/carcadasalborghetti/fragments/VideoFragment.kt
@@ -1,10 +1,13 @@
 package com.ukdev.carcadasalborghetti.fragments
 
+import android.os.Bundle
+import android.view.View
 import com.ukdev.carcadasalborghetti.adapter.VideoAdapter
 import com.ukdev.carcadasalborghetti.handlers.VideoHandler
 import com.ukdev.carcadasalborghetti.model.Video
 import com.ukdev.carcadasalborghetti.utils.provideViewModel
 import com.ukdev.carcadasalborghetti.viewmodel.VideoViewModel
+import kotlinx.android.synthetic.free.fragment_video.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
@@ -14,8 +17,19 @@ class VideoFragment : MediaListFragment<Video>(0, 0) {
     override val viewModel by provideViewModel(VideoViewModel::class)
     override val adapter = VideoAdapter()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bt_paid_version.setOnClickListener {
+            showPaidVersion()
+        }
+    }
+
     override fun onStartPlayback() { }
 
     override fun onStopPlayback() { }
+
+    private fun showPaidVersion() {
+
+    }
 
 }
