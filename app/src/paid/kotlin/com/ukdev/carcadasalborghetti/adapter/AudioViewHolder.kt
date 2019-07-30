efd6a5521fb7f8f1b407675f4fa66e8237cff981
Alan Camargo <alan.uk.br@gmail.com>
2019-07-30 15:48:16 +0100

    Create flavour based view holders

---
+++ app/src/paid/kotlin/com/ukdev/carcadasalborghetti/adapter/AudioViewHolder.kt
@@ -0,0 +1,12 @@
+package com.ukdev.carcadasalborghetti.adapter
+
+import android.view.View
+import com.ukdev.carcadasalborghetti.model.Media
+
+class AudioViewHolder(itemView: View) : MediaViewHolder(itemView) {
+
+    override fun bindTo(media: Media) {
+        // TODO
+    }
+
+}
--- app/src/paid/kotlin/com/ukdev/carcadasalborghetti/model/Media.kt
+++ app/src/paid/kotlin/com/ukdev/carcadasalborghetti/model/Media.kt
@@ -1,13 +1,11 @@
 package com.ukdev.carcadasalborghetti.model
 
-import android.net.Uri
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class Media(
         @Expose @SerializedName("path_lower") val path: String,
         @Expose @SerializedName("name") val title: String,
-        val length: String,
         var position: Int,
-        val uri: Uri
+        var uri: String
 )
