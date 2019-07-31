b2813dc68614b8ade764fac90e57c7473dc410ca
Alan Camargo <alan.uk.br@gmail.com>
2019-07-31 15:45:14 +0100

    Implement video layout

--- app/src/paid/kotlin/com/ukdev/carcadasalborghetti/adapter/VideoViewHolder.kt
+++ app/src/paid/kotlin/com/ukdev/carcadasalborghetti/adapter/VideoViewHolder.kt
@@ -1,12 +1,18 @@
 package com.ukdev.carcadasalborghetti.adapter
 
 import android.view.View
+import android.widget.TextView
+import com.ukdev.carcadasalborghetti.R
 import com.ukdev.carcadasalborghetti.model.Media
 
 class VideoViewHolder(itemView: View) : MediaViewHolder(itemView) {
 
+    private val txtTitle = itemView.findViewById<TextView>(R.id.txt_title)
+
     override fun bindTo(media: Media) {
-        // TODO
+        txtTitle.text = itemView.context.getString(
+                R.string.title_format, media.position, media.title
+        ).removeSuffix(".mp4")
     }
 
 }
