6011bf56198869a9d8c89e8fec88af3c10df7ecf
Alan Camargo <alan.uk.br@gmail.com>
2019-08-06 08:58:51 +0100

    Fix field name in MediaRequest

--- app/src/paid/kotlin/com/ukdev/carcadasalborghetti/api/requests/MediaRequest.kt
+++ app/src/paid/kotlin/com/ukdev/carcadasalborghetti/api/requests/MediaRequest.kt
@@ -2,7 +2,7 @@ package com.ukdev.carcadasalborghetti.api.requests
 
 import com.google.gson.Gson
 
-data class MediaRequest(val id: String) {
+data class MediaRequest(val path: String) {
 
     override fun toString(): String {
         return Gson().toJson(this)
