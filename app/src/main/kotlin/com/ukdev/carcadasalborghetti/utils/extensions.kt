ffba6140316066fe0543e3aff620f32b5dd67b2e
Alan Camargo <alan.uk.br@gmail.com>
2020-02-15 22:24:41 +0000

    Fix sharing bug

--- app/src/main/kotlin/com/ukdev/carcadasalborghetti/utils/extensions.kt
+++ app/src/main/kotlin/com/ukdev/carcadasalborghetti/utils/extensions.kt
@@ -1,12 +1,6 @@
 package com.ukdev.carcadasalborghetti.utils
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-import android.annotation.TargetApi
 import android.content.Context
-import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.os.Build.VERSION_CODES.M
-import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
@@ -20,18 +14,3 @@ fun <T: ViewModel> Fragment.provideViewModel(clazz: KClass<T>) = lazy {
 fun Context.getAppName(): String = getString(R.string.app_name)
 
 fun Context.getAppVersion(): String = packageManager.getPackageInfo(packageName, 0).versionName
-
-@TargetApi(M)
-fun Fragment.hasStoragePermissions(): Boolean {
-    return hasPermission(READ_EXTERNAL_STORAGE) && hasPermission(WRITE_EXTERNAL_STORAGE)
-}
-
-@TargetApi(M)
-fun Fragment.requestStoragePermissions(requestCode: Int) {
-    requestPermissions(arrayOf(READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE), requestCode)
-}
-
-@TargetApi(M)
-private fun Fragment.hasPermission(permission: String): Boolean {
-    return checkSelfPermission(requireContext(), permission) == PERMISSION_GRANTED
-}
