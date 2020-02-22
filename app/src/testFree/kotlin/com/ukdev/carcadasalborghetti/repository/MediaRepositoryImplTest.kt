4455d8847aa46f1dab1cf1a1ccf447fd8b63e88b
Alan Camargo <alan.uk.br@gmail.com>
2020-02-22 00:54:18 +0000

    Set up test environment

---
+++ app/src/testFree/kotlin/com/ukdev/carcadasalborghetti/repository/MediaRepositoryImplTest.kt
@@ -0,0 +1,17 @@
+package com.ukdev.carcadasalborghetti.repository
+
+import org.junit.Before
+import org.junit.Test
+
+// TODO
+class MediaRepositoryImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getMedia() {
+    }
+
+}
