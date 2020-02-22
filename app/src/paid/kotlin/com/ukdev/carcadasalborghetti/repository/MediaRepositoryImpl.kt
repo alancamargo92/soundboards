c763c1d7879f7439ef5f83dcc0f9c832985f3e0b
Alan Camargo <alan.uk.br@gmail.com>
2020-02-22 00:26:26 +0000

    Tiny change

--- app/src/paid/kotlin/com/ukdev/carcadasalborghetti/repository/MediaRepositoryImpl.kt
+++ app/src/paid/kotlin/com/ukdev/carcadasalborghetti/repository/MediaRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package com.ukdev.carcadasalborghetti.repository
 
 import com.ukdev.carcadasalborghetti.api.DropboxApi
-import com.ukdev.carcadasalborghetti.api.tools.ApiProvider
 import com.ukdev.carcadasalborghetti.api.requests.MediaRequest
+import com.ukdev.carcadasalborghetti.api.tools.ApiProvider
 import com.ukdev.carcadasalborghetti.model.*
 import com.ukdev.carcadasalborghetti.utils.CrashReportManager
 import kotlinx.coroutines.Dispatchers
@@ -15,14 +15,13 @@ class MediaRepositoryImpl(
         private val apiProvider: ApiProvider
 ) : MediaRepository(crashReportManager) {
 
-    private val api by lazy { apiProvider.getDropboxService() }
-
     override suspend fun getMedia(mediaType: MediaType): Result<List<Media>> {
         val dir = if (mediaType == MediaType.AUDIO)
             DropboxApi.DIR_AUDIO
         else
             DropboxApi.DIR_VIDEO
         val request = MediaRequest(dir)
+        val api = apiProvider.getDropboxService()
 
         return try {
             val media = withContext(Dispatchers.IO) {
