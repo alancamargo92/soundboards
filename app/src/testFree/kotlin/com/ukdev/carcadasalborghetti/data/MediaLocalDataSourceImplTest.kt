50e57c531114fedcaf47c1263261f8ddfe7ec087
Alan Camargo <alan.uk.br@gmail.com>
2020-02-24 18:41:49 +0000

    Update Gradle

---
+++ app/src/testFree/kotlin/com/ukdev/carcadasalborghetti/data/MediaLocalDataSourceImplTest.kt
@@ -0,0 +1,21 @@
+package com.ukdev.carcadasalborghetti.data
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MediaLocalDataSourceImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getTitles() {
+    }
+
+    @Test
+    fun getAudioUris() {
+    }
+}
