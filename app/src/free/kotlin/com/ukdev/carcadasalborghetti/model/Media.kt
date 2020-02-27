a86d88b82ae5728b85fc9a7575e26f9894f9d4d8
Alan Camargo <alan.uk.br@gmail.com>
2020-02-27 21:04:29 +0000

    Sharing on free flavour fixed

--- app/src/free/kotlin/com/ukdev/carcadasalborghetti/model/Media.kt
+++ app/src/free/kotlin/com/ukdev/carcadasalborghetti/model/Media.kt
@@ -10,15 +10,14 @@ data class Media(val title: String, val uri: Uri) {
         private const val FILE_NAME_SEPARATOR = '#'
 
         fun composeFileName(media: Media): String {
-            return "${media.uri}$FILE_NAME_SEPARATOR${media.position}$FILE_NAME_SEPARATOR${media.title}.mp3"
+            return "${media.position}$FILE_NAME_SEPARATOR${media.title}.mp3"
         }
 
         fun fromFileName(fileName: String): Media {
             val parts = fileName.split(FILE_NAME_SEPARATOR)
-            val uri = Uri.parse(parts[0])
-            val position = parts[1].toInt()
-            val title = parts[2]
-            return Media(title, uri).also {
+            val position = parts[0].toInt()
+            val title = parts[1]
+            return Media(title, Uri.EMPTY).also {
                 it.position = position
             }
         }
